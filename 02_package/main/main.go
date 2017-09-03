dbbf48b6a7568a3e9b51ea291bf94bea96d55808
harkce <habibgarut@gmail.com>
2017-09-04 04:22:54 +0700

    Package

---
+++ 02_package/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/harkce/golang-training/02_package/stringutil"
+	"github.com/harkce/golang-training/02_package/icomefromalaska"
+	//someAlias "github.com/harkce/golang-training/02_package/icomefromalaska"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
