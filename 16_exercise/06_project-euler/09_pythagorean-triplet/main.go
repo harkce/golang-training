e10f98af4ce67e81bf41075e24149cc5041ae523
harkce <habibgarut@gmail.com>
2017-09-20 15:36:17 +0700

    Exercise 2

---
+++ 16_exercise/06_project-euler/09_pythagorean-triplet/main.go
@@ -0,0 +1,37 @@
+// Special Pythagorean triplet
+// Problem 9
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for
+// which,
+//
+// a^2 + b^2 = c^2
+// For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+//
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+func tripletSum(n int) int {
+	i := 1
+	for i < n {
+		j := 1
+		for j < n - i {
+			k := n - i - j
+			if math.Pow(float64(i), 2) + math.Pow(float64(j), 2) == math.Pow(float64(k), 2) {
+				return i * j * k
+			}
+			j++
+		}
+		i++
+	}
+	return 0
+}
+
+func main() {
+	fmt.Println(tripletSum(1000))
+}
