7945fe64ea3aeea0faad783c8e9d790776175655
harkce <habibgarut@gmail.com>
2017-11-26 06:37:34 +0700

    Squaring number

---
+++ 22_go-routines/12_channel-pipeline/01_sq-output/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := gen(2, 3)
+	out := sq(c)
+	for n := range out {
+		fmt.Println(n)
+	}
+}
+
+func gen(nums ...int) chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(c chan int) chan int {
+	out := make(chan int)
+	go func() {
+		for n := range c {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
